e1650c7eced3176ff88ec5c5f49e85c0e843a942
bhardwaj-harshal12 <123346535+bhardwaj-harshal12@users.noreply.github.com>
2023-02-09 10:10:29 +0530

    Add files via upload

---
+++ code c.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+int main(){
+//	int i,n,s=0;
+//	printf("Enter the no. to be taken from keyboard = ");
+//	scanf("%d",&n);
+//	for (i=1;i<=n;i++){
+//		printf("Enter the %d number = ",i);
+//		int a=0;
+//		scanf("%d",&a);
+//		s=s+a;
+//		}printf
+//	printf("The sum is %d \n",s);
+//	printf("The average  is %d",s/10);
+//	int i,n;
+//	printf("Enter the no. of terms  to be taken from keyboard = ");
+//	scanf("%d",&n);
+//	for (i=1;i<=n;i++){
+//		printf("\nEnter the %d number = ",i);
+//		int a=0;
+//		scanf("%d",&a);
+//		printf("The cube of %d is = %d\n",a, a*a*a);
+//		
+//		}
+//	printf("Harshal");
+//	int n,a=1,s=0,i;
+//	printf("Enter the number of terms to be printed = ");
+//	scanf("%d",&n);
+//	for (i=1;i<=n;i++){
+//		printf("%d ",a);
+//		s=s+a;
+//		a=a+2;
+//		
+//	}
+//	printf("\nThe sum is %d",s);
+//	int a=10;
+//	while (a>=2){
+//		printf("%d ",a);
+//		a=a-2;
+//	}
+//	int x=-1;
+//	while (x++==1){
+//		printf("YEs");
+//		
+//	}
+//	int x=0;
+//	while(x++==1){
+//		printf("violet");
+//	}
+//	printf("The value has increamented afterwards %d\n",x);
+//	int a=0;
+//	while(++a==1){
+//		printf("Yes");
+//	}
+//	printf("%d",a);
+//	int num,rem;
+//	printf("Enter the number whose sum wants to find ");
+//	scanf("%d",&num);
+//	int s=num%10;
+//	while (num>0){
+//		rem=num%10;
+//		num=num/10;
+//	}
+//	s=s+rem;
+//
+//	printf("The sum is %d",s);
+//	double f = 21.6666;
+//	printf("%0.2f",f);//%lf
+//	printf("\n%0.4f",f);
+//	printf("\n%.6f \n",f);
+//	printf("%lf",f);
+//	for (int i=0;1;i++){ //condition always true because 1 given , if 4 also then also
+//		printf("%d\n",i);
+//	}
+	int i=0;
+	int max=5;
+	for (;i<max;i++){
+		printf("%d\n",i);
+	}
+	
+	return 0;
+}
